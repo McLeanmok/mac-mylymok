0cfd6f6dc368460df4b8b6bc92836b1a15458842
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2024-07-16 19:39:16 -0700

    Bug fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,12 @@
-
 // Header
-// dynamically displaying the active class 
-const links = document.querySelector('#nav');
-const navLinks = links.querySelectorAll("a"); 
+// dynamically displaying the active class
+const links = document.querySelector("#nav");
+const navLinks = links.querySelectorAll("a");
 const currentURL = window.location.href;
 navLinks.forEach((link) => {
-    if (link.href === currentURL) {
-  link.classList.add("current");
-}
+  if (link.href === currentURL) {
+    link.classList.add("current");
+  }
 });
 // Mobile Menu
 // Open Navigation Menu
@@ -29,3 +28,8 @@ function closeMenu() {
   burger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+// Dynamically change date in footer
+document.querySelector("#year").textContent = new Date()
+  .getFullYear()
+  .toString();
